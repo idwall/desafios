ad83936be2643a22e73a199b4faba9d4273e5644
Renata Abreu <renata89abreu@gmail.com>
2020-08-23 22:25:36 -0300

    small fixes and pom.xml updated

--- crawlers/desafio02-telegram/src/main/java/telegram/IdwallBot.java
+++ crawlers/desafio02-telegram/src/main/java/telegram/IdwallBot.java
@@ -20,7 +20,7 @@ public class IdwallBot extends TelegramLongPollingBot {
                     if (update.getMessage().getText().split(" ").length > 1)
                         NadaPraFazer(update, update.getMessage().getText().split(" ")[1]);
                     else
-                        NadaPraFazer(update, null);
+                        NadaPraFazer(update);
                     break;
                 case "/ABOUTME":
                     aboutMessage(update);
@@ -40,6 +40,32 @@ public class IdwallBot extends TelegramLongPollingBot {
         return BotConfig.BOT_TOKEN;
     }
 
+    public void NadaPraFazer(Update update) {
+        final String REDDIT_URL = "https://old.reddit.com/";
+        try {
+            Document doc = Jsoup.connect(REDDIT_URL).get();
+            Elements score = doc.select("div.score.likes");
+            Elements subreddit = doc.select("a.subreddit.hover.may-blank");
+            Elements title = doc.select("a.title.may-blank");
+            Elements comments = doc.select("a.bylink.comments");
+
+            for (int i = 0; i < subreddit.size(); i++) {
+                String message = score.get(i).text() + " | " + subreddit.get(i).text() + " | " + title.get(i).text() + "\n";
+                if (title.get(i).attr("href").charAt(0) == '/')
+                    message = message + "https://old.reddit.com" + title.get(i).attr("href") + "\n";
+                else
+                    message = message + title.get(i).attr("href") + "\n";
+                message = message + comments.get(i).attr("href");
+
+                this.sendMessage(update.getMessage().getChatId(), message);
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void NadaPraFazer(Update update, String argsSub) {
         final String REDDIT_URL = "https://old.reddit.com/";
         try {
@@ -49,61 +75,34 @@ public class IdwallBot extends TelegramLongPollingBot {
             Elements title = doc.select("a.title.may-blank");
             Elements comments = doc.select("a.bylink.comments");
 
-            if(argsSub != null) {
-                List<String> subs = new ArrayList<>();
-                for(int i = 0; i < argsSub.split(";").length ; i++)
-                    subs.add(argsSub.split(";")[i].toLowerCase());
+            List<String> subs = new ArrayList<>();
+            for(int i = 0; i < argsSub.split(";").length ; i++)
+                subs.add(argsSub.split(";")[i].toLowerCase());
 
-                for (int i = 0; i < subreddit.size(); i++) {
-                    if (subs.contains(subreddit.get(i).text().split("/")[1].toLowerCase())) {
-                        String msg = score.get(i).text() + " | " + subreddit.get(i).text() + " | " + title.get(i).text() + "\n";
-                        if (title.get(i).attr("href").charAt(0) == '/')
-                            msg = msg + "https://old.reddit.com" + title.get(i).attr("href") + "\n";
-                        else
-                            msg = msg + title.get(i).attr("href") + "\n";
-                        msg = msg + comments.get(i).attr("href");
-
-                        this.sendMessage(update.getMessage().getChatId(), msg);
-                    }
-                }
-            } else {
-                for (int i = 0; i < subreddit.size(); i++) {
-                    String msg = score.get(i).text() + " | " + subreddit.get(i).text() + " | " + title.get(i).text() + "\n";
+            for (int i = 0; i < subreddit.size(); i++) {
+                if (subs.contains(subreddit.get(i).text().split("/")[1].toLowerCase())) {
+                    String message = score.get(i).text() + " | " + subreddit.get(i).text() + " | " + title.get(i).text() + "\n";
                     if (title.get(i).attr("href").charAt(0) == '/')
-                        msg = msg + "https://old.reddit.com" + title.get(i).attr("href") + "\n";
+                        message = message + "https://old.reddit.com" + title.get(i).attr("href") + "\n";
                     else
-                        msg = msg + title.get(i).attr("href") + "\n";
-                    msg = msg + comments.get(i).attr("href");
+                        message = message + title.get(i).attr("href") + "\n";
+                    message = message + comments.get(i).attr("href");
 
-                    this.sendMessage(update.getMessage().getChatId(), msg);
+                    this.sendMessage(update.getMessage().getChatId(), message);
                 }
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void usageMessage(Update update) {
-        SendMessage message = new SendMessage()
-                .setChatId(update.getMessage().getChatId())
-                .setText("Comandos disponívels:\n\n/NadaPraFazer\n/NadaPraFazer sub\n/NadaPraFazer sub1;sub2\n/AboutMe");
-        try {
-            execute(message);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
+        this.sendMessage(update.getMessage().getChatId(), "Comandos disponíveis:\n\n/NadaPraFazer\n/NadaPraFazer subreddit\n/NadaPraFazer subreddit1;subreddit2\n/AboutMe");
+
     }
 
     public void aboutMessage(Update update) {
-        SendMessage message = new SendMessage()
-                .setChatId(update.getMessage().getChatId())
-                .setText("https://www.linkedin.com/in/renata89abreu/\nhttps://github.com/rabreu/");
-        try {
-            execute(message);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
+        this.sendMessage(update.getMessage().getChatId(), "https://www.linkedin.com/in/renata89abreu/\nhttps://github.com/rabreu/");
     }
 
     public void sendMessage(Long chatId, String msg) {
